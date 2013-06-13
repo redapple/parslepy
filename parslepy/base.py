f5e473acd4be69e1ec1ea6ede74b8a4b87cec94d
Paul Tremberth <paul.tremberth@gmail.com>
2013-06-13 18:49:05 +0200

    Seperate the selector handling and the Parsley tree extraction.
    Also, allow parsing of a webpage or (HTML) file directly

--- parslepy/base.py
+++ parslepy/base.py
@@ -13,6 +13,18 @@ def xpathtostring(context, nodes):
 def xpathtostringnl(context, nodes):
     return tostringnl(nodes)
 
+# ----------------------------------------------------------------------
+
+# compiled Parsley scripts look like this
+# ParsleyNode(
+#       ParsleyContext(key, options[, Selector]): ParsleyNode(...),
+#           ...or
+#       ParsleyContext(key, options[, Selector]): Selector,
+#       ...)
+
+class ParsleyNode(dict):
+    pass
+
 class ParsleyContext(object):
     def __init__(self, key, operator=None, required=None, scope=None, iterate=False):
         self.key = key
@@ -25,22 +37,147 @@ class ParsleyContext(object):
         return u"<ParsleyContext: k=%s; op=%s; required=%s; scope=%s; iter=%s>" % (
             self.key, self.operator, self.required, self.scope, self.iterate)
 
+class Selector(object):
+    def __init__(self, selector):
+        self.selector = selector
+
+class selector_handlerandler(object):
+    DEBUG = True
+
+    def make(self):
+        raise NotImplemented
+
+    def select(self, document):
+        raise NotImplemented
+
+    def extract(self):
+        raise NotImplemented
+
+class Defaultselector_handlerandler(selector_handlerandler):
+
+    XPATH_EXTENSIONS = {
+        ('parsley', 'str') : xpathtostring,
+        ('parsley', 'strnl') : xpathtostringnl,
+        ('parsley', 'nl') : xpathtostringnl,
+    }
+    EXSLT_NAMESPACES={
+        'math': 'http://exslt.org/math',
+        're': 'http://exslt.org/regular-expressions',
+        'str': 'http://exslt.org/strings',
+    }
+
+
+    @classmethod
+    def _add_parsley_extension_functions(cls, namespace_dict):
+        namespace_dict.update({
+            'prsl' : 'parsley',
+            'parsley' : 'parsley'
+        })
+        return namespace_dict
+
+    REGEX_ENDING_ATTRIBUTE = re.compile(r'^(?P<expr>.+)\s+(?P<attr>@[\w_\d]+)$')
+    @classmethod
+    def make(cls, selection):
+
+        namespaces = cls.EXSLT_NAMESPACES
+        cls._add_parsley_extension_functions(namespaces)
+        try:
+            # CSS with attribute? (non-standard but convenient)
+            # construct CSS selector and append attribute to XPath expression
+            m = cls.REGEX_ENDING_ATTRIBUTE.match(selection)
+            if m:
+                cssselector = m.group("expr")
+                attribute = m.group("attr")
+                cssxpath = lxml.cssselect.CSSSelector(cssselector).path
+                selector = lxml.etree.XPath("%s/%s" % (cssxpath, attribute))
+            else:
+                selector = lxml.cssselect.CSSSelector(selection)
+
+        except Exception as e:
+            if cls.DEBUG:
+                print selection, "is not a CSS selector"
+                print str(e)
+            try:
+                selector = lxml.etree.XPath(selection,
+                    namespaces = namespaces,
+                    extensions = cls.XPATH_EXTENSIONS)
+            except Exception as e:
+                if cls.DEBUG:
+                    print selection, "is not a XPath selector"
+                    print str(e)
+                return None
+        # wrap it
+        return Selector(selector)
+
+    @classmethod
+    def select(cls, document, selector):
+        try:
+            return selector.selector(document)
+        except Exception, e:
+            print str(e)
+            return
+
+    def extract(self, document, parselet_node, debug_offset=''):
+        selected = self.select(document, parselet_node)
+        if selected:
+            if self.DEBUG:
+                print debug_offset, selected
+            if isinstance(selected, (list, tuple)):
+
+                # try decoding to a string if no text() or prsl:str() has been used
+                try:
+                    retval = tostring(selected)
+                    if self.DEBUG:
+                        print debug_offset, "return", retval
+                    return retval
+
+                # assume the selection is already a string(-list)
+                except Exception as e:
+                    if self.DEBUG:
+                        print debug_offset, "tostring failed:", str(e)
+                        print debug_offset, "return", selected
+                    return selected
+            else:
+                if self.DEBUG:
+                    print debug_offset, "selected is not a list; return", selected
+                return selected
+
+        # selector did not match anything
+        else:
+            if self.DEBUG:
+                print debug_offset, "selector did not match anything; return", {}
+            return None
+
 
 class ParsleyExtractor(object):
 
     DEBUG = False
     SPECIAL_LEVEL_KEY = "--"
 
-    def __init__(self, parselet, debug=False):
+    def __init__(self, parselet, selector_handler=None, debug=False):
         if debug:
             self.DEBUG = True
         self.parselet =  parselet
+
+        if not selector_handler:
+            self.selector_handler = Defaultselector_handlerandler()
+
+        elif not(isinstance(selector_handlerandler)):
+            raise ValueError("You must provide a selector_handlerandler instance")
+
+        else:
+            self.selector_handler = selector_handler
+
         self.compile()
 
     @classmethod
     def from_jsonfile(cls, fp, debug=False):
         import json
         return cls(json.load(fp), debug=debug)
+
+    def parse(self, f):
+        doc = lxml.html.parse(f).getroot()
+        return self.extract(doc)
 
     def compile(self):
         self.parselet_tree = self._compile(self.parselet)
@@ -56,7 +193,7 @@ class ParsleyExtractor(object):
                 self.__class__.__name__, parselet_node)
 
         if isinstance(parselet_node, dict):
-            parselet_tree = {}
+            parselet_tree = ParsleyNode()
             for k, v in parselet_node.iteritems():
 
                 m = self.REGEX_PARSELET_KEY.match(k)
@@ -86,7 +223,7 @@ class ParsleyExtractor(object):
                     key,
                     operator=operator,
                     required=key_required,
-                    scope=self.make_selector(scope) if scope else None,
+                    scope=self.selector_handler.make(scope) if scope else None,
                     iterate=iterate)
 
                 if self.DEBUG:
@@ -102,15 +239,8 @@ class ParsleyExtractor(object):
 
         # a string leaf should match some kind of selector
         elif isinstance(parselet_node, basestring):
-            return self.make_selector(parselet_node)
+            return self.selector_handler.make(parselet_node)
 
-    @classmethod
-    def select(cls, document, selector):
-        try:
-            return selector(document)
-        except Exception, e:
-            print str(e)
-            return
 
     def extract(self, document):
         return self._extract(self.parselet_tree, document)
@@ -120,16 +250,17 @@ class ParsleyExtractor(object):
         if self.DEBUG:
             debug_offset = "".join(["    " for x in range(level)])
 
-        if isinstance(parselet_node, dict):
+        if isinstance(parselet_node, ParsleyNode):
 
             output = {}
 
             for ctx, v in parselet_node.iteritems():
                 if self.DEBUG:
                     print debug_offset, "context:", ctx, v
+
                 if ctx.scope:
                     extracted = []
-                    selected = self.select(document, ctx.scope)
+                    selected = self.selector_handler.select(document, ctx.scope)
                     if selected:
                         for i, elem in enumerate(selected, start=1):
                             parse_result = self._extract(v, elem, level=level+1)
@@ -160,7 +291,6 @@ class ParsleyExtractor(object):
                     if self.DEBUG:
                         print str(e)
                         print debug_offset, "error getting first element"
-                        #print output[key]
 
                 # special key to extract hierarchically
                 # but still output at same level
@@ -171,88 +301,7 @@ class ParsleyExtractor(object):
                     output[ctx.key] = extracted
             return output
 
-        # string leaf should match some kind of selector
-        elif isinstance(parselet_node, (lxml.etree.XPath, lxml.cssselect.CSSSelector)):
-            selected = self.select(document, parselet_node)
-            if selected:
-                if self.DEBUG:
-                    print debug_offset, selected
-                if isinstance(selected, (list, tuple)):
+        elif isinstance(parselet_node, Selector):
+            return self.selector_handler.extract(document, parselet_node)
 
-                    # try decoding to a string if no text() or prsl:str() has been used
-                    try:
-                        retval = tostring(selected)
-                        if self.DEBUG:
-                            print debug_offset, "return", retval
-                        return retval
 
-                    # assume the selection is already a string(-list)
-                    except Exception as e:
-                        if self.DEBUG:
-                            print debug_offset, "tostring failed:", str(e)
-                            print debug_offset, "return", selected
-                        return selected
-                else:
-                    if self.DEBUG:
-                        print debug_offset, "selected is not a list; return", selected
-                    return selected
-
-            # selector did not match anything
-            else:
-                if self.DEBUG:
-                    print debug_offset, "selector did not match anything; return", {}
-                return {}
-
-    XPATH_EXTENSIONS = {
-        ('parsley', 'str') : xpathtostring,
-        ('parsley', 'strnl') : xpathtostringnl,
-        ('parsley', 'nl') : xpathtostringnl,
-    }
-    EXSLT_NAMESPACES={
-        'math': 'http://exslt.org/math',
-        're': 'http://exslt.org/regular-expressions',
-        'str': 'http://exslt.org/strings',
-    }
-
-    @classmethod
-    def _add_parsley_extension_functions(cls, namespace_dict):
-        namespace_dict.update({
-            'prsl' : 'parsley',
-            'parsley' : 'parsley'
-        })
-        return namespace_dict
-
-    REGEX_ENDING_ATTRIBUTE = re.compile(r'^(?P<expr>.+)\s+(?P<attr>@[\w_\d]+)$')
-    @classmethod
-    def make_selector(cls, selection):
-        namespaces = cls.EXSLT_NAMESPACES
-        cls._add_parsley_extension_functions(namespaces)
-        try:
-            # CSS with attribute? (non-standard but convenient)
-            # construct CSS selector and append attribute to XPath expression
-            m = cls.REGEX_ENDING_ATTRIBUTE.match(selection)
-            if m:
-                cssselector = m.group("expr")
-                attribute = m.group("attr")
-                #if cls.DEBUG:
-                    #print "CSS selector:", cssselector
-                    #print "attribute:", attribute
-                cssxpath = lxml.cssselect.CSSSelector(cssselector).path
-                selector = lxml.etree.XPath("%s/%s" % (cssxpath, attribute))
-            else:
-                selector = lxml.cssselect.CSSSelector(selection)
-
-        except Exception as e:
-            if cls.DEBUG:
-                print selection, "is not a CSS selector"
-                print str(e)
-            try:
-                selector = lxml.etree.XPath(selection,
-                    namespaces = namespaces,
-                    extensions = cls.XPATH_EXTENSIONS)
-            except Exception as e:
-                if cls.DEBUG:
-                    print selection, "is not a XPath selector"
-                    print str(e)
-                return None
-        return selector
--- run_parslepy.py
+++ run_parslepy.py
@@ -11,6 +11,7 @@ def main():
     parser = optparse.OptionParser()
     parser.add_option("--debug", dest="debug", action="store_true", help="debug mode", default=False)
     parser.add_option("--url", dest="url", help="fetch this URL", default=None)
+    parser.add_option("--file", dest="inputfile", help="parse this HTML file", default=None)
     parser.add_option("--script", dest="parselet", help="Parsley script filename", default=None)
 
     (options, args) = parser.parse_args()
@@ -18,15 +19,14 @@ def main():
     if not options.parselet:
         print "You must provide a Parsley script"
         return
-    if not options.url:
+    if not options.url and not options.inputfile:
         print "You must provide an URL"
         return
 
-    root = lxml.html.parse(options.url).getroot()
-
     with open(options.parselet) as fp:
+
         extractor = parslepy.ParsleyExtractor.from_jsonfile(fp, options.debug)
-        output = extractor.extract(root)
+        output = extractor.parse(options.url or options.inputfile)
         pprint.pprint(output)
 
 if __name__ == '__main__':
